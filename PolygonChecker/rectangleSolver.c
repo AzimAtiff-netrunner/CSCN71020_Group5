dba80b32ee62297fd0c6898647f00552deeac081
Yash <yash.b@live.ca>
2024-11-27 13:05:16 -0500

    added function to calculate distance between points

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -5,6 +5,10 @@
 #include "rectangleSolver.h"
 
 
+// Function to calculate the distance between two points
+float calculateDistance(float point1[2], float point2[2]) {
+    return sqrt(pow(point2[0] - point1[0], 2) + pow(point2[1] - point1[1], 2));
+}
 
 // Function to get the points for the rectangle
 float(*getRectanglePoints(float rectanglePoints[4][2]))[2] {
--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -4,5 +4,6 @@
 
 
 float(*getRectanglePoints(float rectanglePoints[4][2]))[2];	//declare getRectanglePoints to accept the (x,y) pair
+float calculateDistance(float point1[2], float point2[2]);	//calculate distance from all points
 
 #endif
