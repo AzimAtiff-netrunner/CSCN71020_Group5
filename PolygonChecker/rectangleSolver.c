a6319edb3bac8476d1519461af7aae6b6b00c582
Yash <yash.b@live.ca>
2024-11-27 13:07:26 -0500

    added function to check if vector points are perpendicular

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -10,6 +10,21 @@ float calculateDistance(float point1[2], float point2[2]) {
     return sqrt(pow(point2[0] - point1[0], 2) + pow(point2[1] - point1[1], 2));
 }
 
+// Function to check if two vectors are perpendicular
+bool isPerpendicular(float point1[2], float point2[2], float point3[2]) {
+
+    float dx1 = point2[0] - point1[0];  //first (x,y) pair
+
+    float dy1 = point2[1] - point1[1];  //second (x,y) pair
+
+    float dx2 = point3[0] - point2[0];  //third (x,y) pair
+
+    float dy2 = point3[1] - point2[1];  //fourth (x,y) pair
+
+    // Dot product of the vectors (0 for perpendicular vectors)
+    return fabs(dx1 * dx2 + dy1 * dy2) < 1e-6;
+}
+
 // Function to get the points for the rectangle
 float(*getRectanglePoints(float rectanglePoints[4][2]))[2] {
     for (int i = 0; i < 4; i++) {
--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -5,5 +5,6 @@
 
 float(*getRectanglePoints(float rectanglePoints[4][2]))[2];	//declare getRectanglePoints to accept the (x,y) pair
 float calculateDistance(float point1[2], float point2[2]);	//calculate distance from all points
+bool isPerpendicular(float point1[2], float point2[2], float point3[2]);	//declare if lines are perpendicular
 
 #endif
